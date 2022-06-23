aeca68376a27fd868e1b502121f1a3e291887213
Stanislav <terve038@gmail.com>
2022-06-23 13:37:43 +0300

    was add resonsive vesrion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
+
 $(".navbar a").on("click", function (e) {
   if (this.hash !== "") {
     e.preventDefault();
@@ -53,27 +77,3 @@ $(".navbar a").on("click", function (e) {
 //       }
 //     }
 //   });
-
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.3
-        }s`;
-      }
-    });
-
-    burger.classList.toggle("toggle");
-  });
-};
-
-navSlide();
